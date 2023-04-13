054ab4c5269f9c0893662b0261e330e5cc97297a
henrikDavin <davin.h.github.com>
2023-04-13 14:17:13 -0400

    First Commit

---
+++ AstronautDetails/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  AstronautDetails
+//
+//  Created by Davin Henrik on 4/13/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+
---
+++ AstronautDetails/AstronautDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautDetailsApp.swift
+//  AstronautDetails
+//
+//  Created by Davin Henrik on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AstronautDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
